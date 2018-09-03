42e201deb2c79b5cdabcc6ad5613faf86ee3b0df
Mark Vogt <markevogt@outlook.com>
2018-09-03 15:26:01 -0500

    INITIAL VS .NET Core Console Project Creation...

---
+++ Globalmantics/Globalmantics.BandAgent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Globalmantics.BandAgent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Mark's 2nd attempt at BandAgent!");
+        }
+    }
+}
